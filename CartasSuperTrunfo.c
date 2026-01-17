3fe414f6544798bb77efed501212343320b6664d
zagred7 <166218753+zagred7@users.noreply.github.com>
2026-01-17 20:19:22 +0000

    novaAttSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,95 +1,122 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-  
-  char nome[50] = "madara";
-int idade = 77;
+int main(){
 
-printf("Digite a sua idade: ");
-scanf("%d", &idade);
-printf("A sua idade é: %d\n", idade);
 
-printf("Digite o seu nome: ");
-scanf("%s", &nome);
-printf("O seu nome é: %s\n", nome);
+    char nome[50];
+    int idade;
 
-printf("Agora vamos criar suas cartas!\n");
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+    printf("A sua idade é: %d\n", idade);
 
-char estado1 = 'R';
-char codigo[50] = "RJ";
-char cidade [50] = "RIO DE JANEIRO";
-int populaçao = 500000;
-int turistico = 5000;
-float pib = 30;
-float area = 1500.50;
 
-printf("escolha uma letra para representar o estado: ");
-scanf(" %c", &estado1);
-printf("a letra do estado escolhido foi: %c\n", estado1);
+    printf("Digite o seu nome: ");
+    scanf("%s", &nome);
+    printf("O seu nome é: %s\n", nome);
 
-printf("Qual o nome da cidade da carta um?(exemplo: Rio-de-Janeiro): ");
-scanf("%s", &cidade);
-printf("A cidade da carta um é: %s\n", cidade);
+    printf("Agora vamos criar suas cartas!\n");
 
-printf("crie um codigo para a sua cidade(exemplo: RJ10): ");
-scanf("%s", &codigo);
-printf("O codigo da carta um é: %s\n", codigo);
+    char estado1; 
+    char codigo[50];
+    char cidade [50];
+    int populaçao;
+    int turistico;
+    float pib;
+    float area;
 
-printf("Qual o tamanho da população da sua cidade?(exemplo: 2000 habitantes): ");
-scanf("%d", &populaçao);
-printf("O tamanho da população da cidade é: %d\n", populaçao);
+    printf("escolha uma letra para representar o estado: ");
+    scanf(" %c", &estado1);
+    printf("a letra do estado escolhido foi: %c\n", estado1);
+    printf("Qual o nome da cidade da carta um?(exemplo: Rio-de-Janeiro): ");
+    scanf("%s", &cidade);
+    printf("A cidade da carta um é: %s\n", cidade);
+    printf("crie um codigo para a sua cidade(exemplo: RJ10): ");
+    scanf("%s", &codigo);
+    printf("O codigo da carta um é: %s\n", codigo);
+    printf("Qual o tamanho da população da sua cidade?(exemplo: 2000 habitantes): ");
+    scanf("%d", &populaçao);
+    printf("O tamanho da população da cidade é: %d\n", populaçao);
+    printf("escolha quantos pontos turisticos a sua cidade vai ter(exemplo: 40 pontos turisticos): ");
+    scanf("%d", &turistico);
+    printf("o numero de pontos turisticos da sua cidade é: %d\n", turistico);
+    printf("Qual o PIB da sua cidade?(exemplo: R$ 90.000): ");
+    scanf("%f", &pib);
+    printf("o PIB da sua cidade é R$: %.2f\n", pib);
+    printf("Qual a area da sua cidade?(exemplo: 1500.50): ");
+    scanf("%f", &area);
+    printf("A area da sua cidade é: %.2f\n", area);
 
-printf("escolha quantos pontos turisticos a sua cidade vai ter(exemplo: 40 pontos turisticos): ");
-scanf("%d", &turistico);
-printf("o numero de pontos turisticos da sua cidade é: %d\n", turistico);
+    printf("Agora vamos calcular!!\n");
 
-printf("Qual o PIB da sua cidade?(exemplo: 777.77): ");
-scanf("%f", &pib);
-printf("o PIB da sua cidade é: %.2f\n", pib);
+    float dp = (float) populaçao / area;
+    float pc = (float) pib / populaçao;
 
-printf("Qual a area da sua cidade?(exemplo: 1500.50): ");
-scanf("%f", &area);
-printf("A area da sua cidade é: %.2f\n", area);
+    printf("A Densidade Populacional da carta um e: %.2f\n", dp);
+    printf("o PIB per Capita da carta um e: %.2f\n", pc);
 
-printf("Agora vomos escolher a segunda carta!\n");
+    float superPoder;
+    superPoder = (float)populaçao + area + pib + turistico + pc + dp;
+    printf("o superpoder da carta um e: %.2f\n", superPoder);
 
-char estado2 = 'R';
-char codigo2[50] = "RJ";
-char cidade2 [50] = "RIO DE JANEIRO";
-int populaçao2 = 500000;
-int turistico2 = 5000;
-float pib2 = 30;
-float area2 = 1500.50;
+    printf("Agora vomos escolher a segunda carta!\n");
 
-printf("escolha uma letra para representar o estado: ");
-scanf(" %c", &estado2);
-printf("a letra do estado escolhido foi: %c\n", estado2);
+    char estado2;
+    char codigo2[50];
+    char cidade2 [50];
+    int populaçao2;
+    int turistico2;
+    float pib2;
+    float area2;
 
-printf("Qual o nome da cidade da carta dois?(exemplo: Rio-de-Janeiro): ");
-scanf("%s", &cidade2);
-printf("A cidade da carta um é: %s\n", cidade2);
+    printf("escolha uma letra para representar o estado: ");
+    scanf(" %c", &estado2);
+    printf("a letra do estado escolhido foi: %c\n", estado2);
+    printf("Qual o nome da cidade da carta dois?(exemplo: Rio-de-Janeiro): ");
+    scanf("%s", &cidade2);
+    printf("A cidade da carta um é: %s\n", cidade2);
+    printf("crie um codigo para a sua cidade(exemplo: RJ10): ");
+    scanf("%s", &codigo2);
+    printf("O codigo da carta um é: %s\n", codigo2);
+    printf("Qual o tamanho da população da sua cidade?(exemplo: 2000 habitantes): ");
+    scanf("%d", &populaçao2);
+    printf("O tamanho da população da cidade é: %d\n", populaçao2);
+    printf("escolha quantos pontos turisticos a sua cidade vai ter(exemplo: 40 pontos turisticos): ");
+    scanf("%d", &turistico2);
+    printf("o numero de pontos turisticos da sua cidade é: %d\n", turistico2);
+    printf("Qual o PIB da sua cidade?(exemplo: R$ 90.000): ");
+    scanf("%f", &pib2);
+    printf("o PIB da sua cidade é R$: %.2f\n", pib2);
+    printf("Qual a area da sua cidade?(exemplo: 1500.50km): ");
+    scanf("%f", &area2);
+    printf("A area da sua cidade é: %.2f\n", area2);
 
-printf("crie um codigo para a sua cidade(exemplo: RJ10): ");
-scanf("%s", &codigo2);
-printf("O codigo da carta um é: %s\n", codigo2);
+    printf("Agora vamos calcular!!\n");
 
-printf("Qual o tamanho da população da sua cidade?(exemplo: 2000 habitantes): ");
-scanf("%d", &populaçao2);
-printf("O tamanho da população da cidade é: %d\n", populaçao2);
 
-printf("escolha quantos pontos turisticos a sua cidade vai ter(exemplo: 40 pontos turisticos): ");
-scanf("%d", &turistico2);
-printf("o numero de pontos turisticos da sua cidade é: %d\n", turistico2);
+    float dp2 = (float) populaçao2 / area2;
+    float pc2 = (float) pib2 / populaçao2;
 
-printf("Qual o PIB da sua cidade?(exemplo: 777.77): ");
-scanf("%f", &pib2);
-printf("o PIB da sua cidade é: %.2f\n", pib2);
 
-printf("Qual a area da sua cidade?(exemplo: 1500.50): ");
-scanf("%f", &area2);
-printf("A area da sua cidade é: %.2f\n", area2);
+    printf("A Densidade Populacional da carta um e: %.2f\n", dp2);
+    printf("o PIB per Capita da carta um e: %.2f\n", pc2);
 
-printf("Agora vamos jogar!!\n");
 
-return 0;
-} 
+    float superPoder2;
+    superPoder2 = (float)populaçao2 + area2 + pib2 + turistico2 + pc2 + dp2;
+    printf("o superpoder da carta um e: %.2f\n", superPoder2);
+
+    printf("vamos comparar as cartas e ver qual vence!\n");
+    printf("1 para verdadeiro = Carta 1 vence\n e 0 para falso = Carta 2 vence\n)\n");
+
+    printf("qual populaçao vence: %d\n",(int) populaçao > populaçao2);
+    printf("qual area vence: %d\n",(int) area > area2);
+    printf("qual PIB vence: %d\n",(int) pib > pib2);
+    printf("qual pontos turisticos vence: %d\n",(int) turistico > turistico2);
+    printf("qual Densidade Populacional vence: %d\n",(int) dp < dp2);
+    printf("qual PIB per Capita vence: %d\n",(int) pc > pc2);
+    printf("qual Super Poder vence: %d\n",(int) superPoder > superPoder2);
+
+ return 0; 
+
+}
